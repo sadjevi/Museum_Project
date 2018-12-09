85068099cf93bb7d8caa31b8a76b79041055365a
sadjevi <sadjevi@me.com>
2018-12-09 19:43:55 +0100

    mapping updated & controllers added

--- src/SA/MuseumBundle/Controller/BookingController.php
+++ src/SA/MuseumBundle/Controller/BookingController.php
@@ -1,15 +1,52 @@
 <?php
 
+
 namespace SA\MuseumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BookingController extends Controller
 {
-    public function indeAction()
+    public function viewAction($id)
     {
-        $content = $this->get('templating')->render('SAMuseumBundle:B:index.html.twig');
+        // Ici, on récupérera la resa  correspondante à l'id $id
 
-        return new Response($content);
+        return $this->render('SAPlatformBundle:Booking:view.html.twig', array(
+            'id' => $id
+        ));
     }
+
+    public function addAction(Request $request)
+    {
+
+        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+        if ($request->isMethod('POST')) {
+            // Ici, on s'occupera de la création et de la gestion du formulaire
+
+            $request->getSession()->getFlashBag()->add('notice', 'Reservation bien enregistrée.');
+
+            // Puis on redirige vers la page de visualisation de cettte annonce
+            return $this->redirectToRoute('sa_platform_view', array('id' => 5));
+        }
+
+        // Si on n'est pas en POST, alors on affiche le formulaire
+        return $this->render('SAPlatformBundle:Booking:add.html.twig');
+    }
+
+    /*public function editAction($id, Request $request)
+    {
+        // Ici, on récupérera la resa correspondante à $id
+
+        // Même mécanisme que pour l'ajout
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'reservation bien modifiée.');
+
+            return $this->redirectToRoute('sa_platform_view', array('id' => 5));
+        }
+
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+    }*/
+
 }
--- src/SA/MuseumBundle/Controller/DefaultController.php
+++ src/SA/MuseumBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace SA\MuseumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
+        // On ne sait pas combien de pages il y a
+        // Mais on sait qu'une page doit être supérieure ou égale à 1
+        if ($page < 1) {
+            // On déclenche une exception NotFoundHttpException, cela va afficher
+            // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+            throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+        }
+
+      // pour l'instant, on ne fait qu'appeler le template
         return $this->render('SAMuseumBundle:Default:index.html.twig');
+    }
+
+    public function infosAction()
+    {
+        return $this->render('SAMuseumBundle:Default:infos.html.twig');
+
     }
 }
